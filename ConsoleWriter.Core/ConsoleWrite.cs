0680723fdcf5b97498e81b619f01f5f1041d7108
RMenezes <rafaeldosanjos@gmail.com>
2026-01-18 21:44:11 -0300

    Implementation of Write and WriteLine  methods with ConsoleColor(Foreground/Background) and TextTransform parameters.

--- ConsoleWriter.Core/ConsoleWrite.cs
+++ ConsoleWriter.Core/ConsoleWrite.cs
@@ -4,6 +4,100 @@ namespace ConsoleWriter.Core
 {
     public static class ConsoleWrite
     {
-        // Implementação vem no próximo passo
+        #region GENERIC METHODS
+        public static void Write(
+            string text,
+            ConsoleColor foreground,
+            TextTransform transform = TextTransform.None)
+        {
+            var originalForeground = Console.ForegroundColor;
+
+            try
+            {
+                Console.ForegroundColor = foreground;
+                Console.Write(ApplyTransform(text, transform));
+            }
+            finally
+            {
+                Console.ForegroundColor = originalForeground;
+            }
+        }
+
+        public static void Write(
+            string text,
+            ConsoleColor foreground,
+            ConsoleColor background,
+            TextTransform transform = TextTransform.None)
+        {
+            var originalForeground = Console.ForegroundColor;
+            var originalBackground = Console.BackgroundColor;
+
+            try
+            {
+                Console.ForegroundColor = foreground;
+                Console.BackgroundColor = background;
+                Console.Write(ApplyTransform(text, transform));
+            }
+            finally
+            {
+                Console.ForegroundColor = originalForeground;
+                Console.BackgroundColor = originalBackground;
+            }
+        }
+
+        public static void WriteLine(
+            string text,
+            ConsoleColor foreground,
+            TextTransform transform = TextTransform.None)
+        {
+            var originalForeground = Console.ForegroundColor;
+
+            try
+            {
+                Console.ForegroundColor = foreground;
+                Console.WriteLine(ApplyTransform(text, transform));
+            }
+            finally
+            {
+                Console.ForegroundColor = originalForeground;
+            }
+        }
+
+        public static void WriteLine(
+            string text,
+            ConsoleColor foreground,
+            ConsoleColor background,
+            TextTransform transform = TextTransform.None)
+        {
+            var originalForeground = Console.ForegroundColor;
+            var originalBackground = Console.BackgroundColor;
+
+            try
+            {
+                Console.ForegroundColor = foreground;
+                Console.BackgroundColor = background;
+                Console.WriteLine(ApplyTransform(text, transform));
+            }
+            finally
+            {
+                Console.ForegroundColor = originalForeground;
+                Console.BackgroundColor = originalBackground;
+            }
+        }
+        #endregion
+
+        #region PRIVATE METHODS
+        private static string ApplyTransform(string text, TextTransform transform)
+        {
+            if (text == null)
+                return string.Empty;
+
+            return transform switch
+            {
+                TextTransform.UpperCase => text.ToUpperInvariant(),
+                _ => text
+            };
+        }
+        #endregion
     }
 }
